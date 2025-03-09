59e2f6b0f65f865ab002a80f2813527e8c7f7525
Lars Ahlzen <lars@ahlzen.com>
2025-03-08 21:35:04 -0500

    Added parsing of hex colors.

--- MapLib/Output/ColorUtil.cs
+++ MapLib/Output/ColorUtil.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace MapLib.Output;
 
@@ -17,4 +18,56 @@ public static class ColorUtil
             return "#" + c.R.ToString("x2") + c.G.ToString("x2") + c.B.ToString("x2") + c.A.ToString("x2");
         }
     }
+
+    /// <summary>
+    /// Parses a hex color into a Color structure.
+    /// </summary>
+    /// <remarks>
+    /// Implements the CSS spec, i.e. 3/4/6/8-digit formats:
+    ///  #RGB
+    ///  #RGBA
+    ///  #RRGGBB
+    ///  #RRGGBBAA
+    /// </remarks>
+    /// <exception cref="FormatException">
+    /// Thrown if specified string is not a valid hex code.
+    /// </exception>
+    public static Color FromHex(string hexString)
+    {
+        var match = HexColorRegex.Match(hexString.Trim());
+        if (match.Success)
+        {
+            const System.Globalization.NumberStyles hex = System.Globalization.NumberStyles.HexNumber;
+            string hexDigits = match.Groups[1].Value;
+            switch (hexDigits.Length)
+            {
+                case 3:
+                    return Color.FromArgb(
+                        0x11 * int.Parse(hexDigits.Substring(0, 1), hex),
+                        0x11 * int.Parse(hexDigits.Substring(1, 1), hex),
+                        0x11 * int.Parse(hexDigits.Substring(2, 1), hex));
+                case 4:
+                    return Color.FromArgb(
+                        0x11 * int.Parse(hexDigits.Substring(3, 1), hex),
+                        0x11 * int.Parse(hexDigits.Substring(0, 1), hex),
+                        0x11 * int.Parse(hexDigits.Substring(1, 1), hex),
+                        0x11 * int.Parse(hexDigits.Substring(2, 1), hex));
+                case 6:
+                    return Color.FromArgb(
+                        int.Parse(hexDigits.Substring(0, 2), hex),
+                        int.Parse(hexDigits.Substring(2, 2), hex),
+                        int.Parse(hexDigits.Substring(4, 2), hex));
+                case 8:
+                    return Color.FromArgb(
+                        int.Parse(hexDigits.Substring(6, 2), hex),
+                        int.Parse(hexDigits.Substring(0, 2), hex),
+                        int.Parse(hexDigits.Substring(2, 2), hex),
+                        int.Parse(hexDigits.Substring(4, 2), hex));
+            }
+        }
+        throw new FormatException($"Not a valid hex color: \"{hexString}\"");
+    }
+
+    private static Regex HexColorRegex = new(
+        "#([1-9a-fA-F]{3,8})", RegexOptions.Compiled);
 }
---
+++ MapLibTests/Output/ColorUtilFixture.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using MapLib.Output;
+
+namespace MapLib.Tests.Output;
+
+[TestFixture]
+public class ColorUtilFixture : BaseFixture
+{
+    [Test]
+    public void TestFromHex()
+    {
+        // Three-value syntax: #RGB
+        Color c1 = ColorUtil.FromHex("#734");
+        Assert.That(c1.R == 0x77);
+        Assert.That(c1.G == 0x33);
+        Assert.That(c1.B == 0x44);
+        Assert.That(c1.A == 0xff);
+
+        // Four-value syntax: #RGBA
+        Color c2 = ColorUtil.FromHex("#ABCD");
+        Assert.That(c2.R == 0xaa);
+        Assert.That(c2.G == 0xbb);
+        Assert.That(c2.B == 0xcc);
+        Assert.That(c2.A == 0xdd);
+
+        // Six-value syntax: #RRGGBB
+        Color c3 = ColorUtil.FromHex("#c82365");
+        Assert.That(c3.R == 0xc8);
+        Assert.That(c3.G == 0x23);
+        Assert.That(c3.B == 0x65);
+        Assert.That(c3.A == 0xff);
+
+        // Eight-value syntax: #RRGGBBAA
+        Color c4 = ColorUtil.FromHex("#c8236544");
+        Assert.That(c4.R == 0xc8);
+        Assert.That(c4.G == 0x23);
+        Assert.That(c4.B == 0x65);
+        Assert.That(c4.A == 0x44);
+
+        // Invalid values
+        Assert.Throws<FormatException>(() => ColorUtil.FromHex("123")); // requires #
+        Assert.Throws<FormatException>(() => ColorUtil.FromHex("#ffee8")); // no 5-valued format
+        Assert.Throws<FormatException>(() => ColorUtil.FromHex(""));
+    }
+}
