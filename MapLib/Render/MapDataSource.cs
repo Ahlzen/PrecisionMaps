d37da3f2b88b778d8156284747c63680f9bec901
Lars Ahlzen <lars@ahlzen.com>
2025-03-11 06:13:17 -0400

    Factored out map classes.

---
+++ MapLib/Render/MapDataSource.cs
@@ -0,0 +1,38 @@
+﻿using MapLib.DataSources;
+
+namespace MapLib.Render;
+
+public abstract class MapDataSource
+{
+    public string Name { get; }
+    public abstract string Srs { get; }
+
+    public MapDataSource(string name)
+    {
+        Name = name;
+    }
+}
+
+public class VectorMapDataSource : MapDataSource
+{
+    public IVectorDataSource DataSource { get; }
+    public override string Srs => DataSource.Srs;
+
+    public VectorMapDataSource(string name,
+        IVectorDataSource dataSource) : base(name)
+    {
+        DataSource = dataSource;
+    }
+}
+
+public class RasterMapDataSource : MapDataSource
+{
+    public IRasterDataSource DataSource { get; }
+    public override string Srs => DataSource.Srs;
+
+    public RasterMapDataSource(string name,
+        IRasterDataSource dataSource) : base(name)
+    {
+        DataSource = dataSource;
+    }
+}
