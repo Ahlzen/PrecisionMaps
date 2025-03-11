101582a21bac026923c8689473cc2581e26480b9
Lars Ahlzen <lars@ahlzen.com>
2025-03-11 06:21:31 -0400

    Explicit startup object (main) for MapLibTests.

--- MapLibTests/Main.cs
+++ MapLibTests/Main.cs
@@ -1,6 +1,15 @@
 ﻿using BenchmarkDotNet.Running;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 // This is strictly to run launch the benchmark runner
 // in a convenient way as a console app.
 
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+public class MapLibBenchmarks
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
+
+
